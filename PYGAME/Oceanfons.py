03e22ad19745b80e65fd8e8ff49205d4d95f420d
FrancescPous <158268532+FrancescPous@users.noreply.github.com>
2024-03-06 11:05:25 +0100

    Update Oceanfons.py

--- PYGAME/Oceanfons.py
+++ PYGAME/Oceanfons.py
@@ -1,13 +1,14 @@
 # import the pygame module, so you can use it
-import pygame
+import pygame, sys
 
-WIDTH = 320
-HIGH = 200
-BACKGROUND_IMAGE = "assets/tigre.jpg"
+WIDTH = 640
+HIGH = 480
 LOGO_IMAGE = "assets/moon.png"
 CAPTION_TEXT = "My Space Game"
+BACKGROUND_IMAGE = "assets/megalodon.jpg"
 # create a surface on screen that has the size of 640 x 480
 screen = pygame.display.set_mode((WIDTH, HIGH),pygame.FULLSCREEN)
+
 
 # define a main function
 def main():
@@ -21,19 +22,22 @@ def main():
     pygame.display.set_caption(CAPTION_TEXT)
     # define a variable to control the main loop
     background = pygame.image.load(BACKGROUND_IMAGE).convert()
-    screen.blit(background, (0,0))
+    screen.blit(background, (0, 0))
     running = True
     # main loop
     while running:
         # background set to BLACK
-        # screen.fill((75,83,32))
+        # screen.fill((155, 155, 155))
         # draw the screen
         pygame.display.flip()
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+        # poll for events
+        # pygame.QUIT event means the user clicked X to close your window
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                sys.exit()
+
+
 # run the main function only if this module is executed as the main script
 # (if you import this as a module then nothing is executed)
 if __name__ == "__main__":
