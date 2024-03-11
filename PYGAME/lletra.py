7df1f7db9e10048f68f7aaa0f6ddc57cc810cfd8
FrancescPous <158268532+FrancescPous@users.noreply.github.com>
2024-03-11 11:50:49 +0100

    Create lletra.py

---
+++ PYGAME/lletra.py
@@ -0,0 +1,29 @@
+import pygame, sys
+from pygame.locals import *
+
+AMPLE = 600
+ALT = 600
+TAMANY = (AMPLE,ALT)
+NEGRE = (0,0,0)
+VERMELL = (255,0,0,0)
+BLANC = (255,255,255)
+
+pygame.init()
+pantalla = pygame.display.set_mode(TAMANY)
+pygame.display.set_caption('Rectangle')
+while True: # main game loop
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+
+    pantalla.fill(BLANC)
+    rectangle1 =  pygame.Rect(100,100, 200, 50)
+    rectangle2 =  pygame.Rect(100,250,200,50)
+    rectangle3 = pygame.Rect(100, 100,50,400)
+    rectangle4 = pygame.Rect(300, 100, 50, 200)
+    pygame.draw.rect(pantalla, VERMELL, rectangle1)
+    pygame.draw.rect(pantalla, VERMELL, rectangle2)
+    pygame.draw.rect(pantalla, VERMELL, rectangle3)
+    pygame.draw.rect(pantalla, VERMELL, rectangle4)
+    pygame.display.update()
